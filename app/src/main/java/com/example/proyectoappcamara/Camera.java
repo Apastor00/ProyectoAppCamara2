4bd36379f936083c88976aaade1731a504d3264a
Apastor00 <artemipastor@gmail.com>
2022-02-02 20:20:56 +0100

    hey

---
+++ app/src/main/java/com/example/proyectoappcamara/Camera.java
@@ -0,0 +1,54 @@
+package com.example.proyectoappcamara;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class Camera extends AppCompatActivity {
+
+    Button btnCamara;
+    ImageView imgView;
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+
+        btnCamara = findViewById(R.id.btnCamara);
+        imgView = findViewById(R.id.imageView);
+        webView = findViewById(R.id.webView);
+
+        webView.loadUrl("file:///android_asset/el_sol.html");
+
+        btnCamara.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                abrirCamara();
+            }
+        });
+
+    }
+
+    private void abrirCamara() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imgBitmap = (Bitmap) extras.get("data");
+            imgView.setImageBitmap(imgBitmap);
+        }
+    }
+}
--- app/src/main/java/com/example/proyectoappcamara/MainActivity.java
+++ app/src/main/java/com/example/proyectoappcamara/MainActivity.java
@@ -28,33 +28,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnCamara = findViewById(R.id.btnCamara);
-        imgView = findViewById(R.id.imageView);
-        webView = findViewById(R.id.webView);
 
-        webView.loadUrl("file:///android_asset/el_sol.html");
-
-        btnCamara.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                abrirCamara();
-            }
-        });
-
-    }
-
-    private void abrirCamara(){
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, 1);
-    }
-
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1 && resultCode == RESULT_OK) {
-            Bundle extras = data.getExtras();
-            Bitmap imgBitmap = (Bitmap) extras.get("data");
-            imgView.setImageBitmap(imgBitmap);
-        }
     }
 }
